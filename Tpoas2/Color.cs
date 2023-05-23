cb1a61cda4beef83fb76a0914614c323fab3074b
VladimirKosolapov04 <79053977+VladimirKosolapov04@users.noreply.github.com>
2023-05-24 01:08:07 +0700

    Add files via upload

---
+++ Tpoas2/Color.cs
@@ -0,0 +1,20 @@
+﻿namespace Tpoas2
+{
+    public class Color
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public double yarkost(int r, int g, int b)
+        {
+            if (r > 255) r = 255;
+            if (g > 255) g = 255;
+            if (b > 255) b = 255;
+            if (r < 0) r = 0;
+            if (g < 0) g = 0;
+            if (b < 0) b = 0;
+            return 0.3 * r + 0.59 * g + 0.11 * b;
+        }
+    }
+}
